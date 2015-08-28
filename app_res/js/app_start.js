9754f692d7a1fb3f440616a5cd2628192bce622e
Rudi Feijo <rudibr@gmail.com>
2015-08-28 16:15:30 -0300

    asd
    asdasd

--- app_res/js/app_start.js
+++ app_res/js/app_start.js
@@ -41,7 +41,6 @@ function app_connected() {
 }
 
 function onDeviceready() {
-    alert('onDeviceready')
     CP.deviceready = true;
     //https://github.com/apache/cordova-plugin-network-information/blob/df7aac845dc7deddbdb76e89216776a802ee8b67/doc/index.md
     //Applications typically should use document.addEventListener to attach an event listener once the deviceready event fires.
@@ -50,7 +49,6 @@ function onDeviceready() {
 }
 
 function onOnline() {
-    alert('onOnline')
     if (typeof CP.offline_warn_to != 'undefined') {
         window.clearTimeout(CP.offline_warn_to);
     }
@@ -62,8 +60,6 @@ function onOnline() {
 }
 
 function onOffline() {
-    alert('onOffline')
-
     if (typeof CP.offline_warn_to != 'undefined') {
         window.clearTimeout(CP.offline_warn_to);
     }
