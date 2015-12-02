258275418e0bdb73f05e19b6cbdd6a15490bf6b2
Rudi Feijo <rudibr@gmail.com>
2015-12-02 14:57:24 -0200

    fg
    g

--- js/app.js
+++ js/app.js
@@ -17,27 +17,23 @@ app.constant('cfg', {
 
 app.run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
-        alert(1)
+        splash_hide();
+        ionic.Platform.isFullScreen = true;
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
-        if (window.cordova && window.cordova.plugins.Keyboard) {
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
             cordova.plugins.Keyboard.disableScroll(true);
-
         }
-        alert(2)
         if (window.StatusBar) {
             // org.apache.cordova.statusbar required
             StatusBar.styleDefault();
         }
-        alert(3)
-        splash_hide();
-        alert(4)
     });
 });
 
 app.config(function($stateProvider, $urlRouterProvider, localStorageServiceProvider) {
-clog('config')
+    clog('config')
     //para desabilitar o cache por completo : $ionicConfigProvider.views.maxCache(0);
 
     localStorageServiceProvider.setPrefix('pizza');
@@ -105,7 +101,7 @@ clog('config')
 
     $stateProvider.state('app.pedido', {
         url: "/pedido",
-        cache:false,
+        cache: false,
         views: {
             'menuContent': {
                 templateUrl: "/pedido.html",
